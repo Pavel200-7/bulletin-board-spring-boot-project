729bf83f9a56c15f7254f52d58733a5f3560b6f7
Pahan200-7 <pavlik.yakovlev.2000makron@mail.ru>
2026-03-02 17:52:13 +0300

    зависимости

--- service/bulletin/src/main/java/com/example/bulletin/application/service/characteristicvalue/data/response/DeleteCharacteristicValueResponse.java
+++ service/bulletin/src/main/java/com/example/bulletin/application/service/characteristicvalue/data/response/DeleteCharacteristicValueResponse.java
@@ -10,5 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DeleteCharacteristicValueResponse {
+    @Builder.Default
     private boolean succeed = true;
 }
