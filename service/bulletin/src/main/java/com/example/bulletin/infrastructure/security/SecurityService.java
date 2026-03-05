8c84d3490aaf8f16e27d83005cdbf2f4e6739737
Pahan200-7 <pavlik.yakovlev.2000makron@mail.ru>
2026-03-05 22:17:51 +0300

    штука для извлечения клеймов сделана

--- service/bulletin/src/main/java/com/example/bulletin/infrastructure/security/SecurityService.java
+++ service/bulletin/src/main/java/com/example/bulletin/infrastructure/security/SecurityService.java
@@ -1,4 +1,20 @@
 package com.example.bulletin.infrastructure.security;
 
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 public interface SecurityService {
+    Jwt getCurrentJwt();
+    String getCurrentUserId();
+    UUID getCurrentUserIdAsUUID();
+    String getCurrentUsername();
+    String getCurrentEmail();
+    List<String> getCurrentRoles();
+    boolean hasRole(String role);
+    boolean isCurrentUser(UUID userId);
+    Map<String, Object> getAllClaims();
+    <T> T getClaim(String claim, Class<T> type);
 }
