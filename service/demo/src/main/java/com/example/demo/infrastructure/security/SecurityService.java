fd320a05a6cdd76c6e90b5e4d71323c7fe660bd6
Pahan200-7 <pavlik.yakovlev.2000makron@mail.ru>
2026-03-18 22:15:53 +0300

    удален ненужный проект

--- service/demo/src/main/java/com/example/demo/infrastructure/security/SecurityService.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.infrastructure.security;
-
-public interface SecurityService {
-}
--- service/demo/src/main/java/com/example/demo/infrastructure/security/SecurityServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.infrastructure.security;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class SecurityServiceImpl implements SecurityService {
-}
